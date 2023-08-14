b40bd8a8e5b9a9ee7c92f3ce1fbad7fb4ecb1ec6
Jose Montes De Oca Morfin <josemontesdeocamorfin@gmail.com>
2023-08-14 13:01:39 -0500

    Added body part component for scroll bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,11 +7,14 @@ import HeroBanner from '../components/HeroBanner.js'
 
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all');
+  const [exercises, setExercises] = useState([]);
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+      <Exercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
     </Box>
   )
 }
