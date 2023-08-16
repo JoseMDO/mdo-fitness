fbc5ea5758a5954cce841d6a82194a8dfeb4e9ea
Jose Montes De Oca Morfin <josemontesdeocamorfin@gmail.com>
2023-08-16 18:07:23 -0500

    Implemented Favorites Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,10 @@ import { Box } from '@mui/material';
 import Exercises from '../components/Exercises';
 import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
+import Favorites from '../components/Favorites';
 
 const Home = () => {
+  const [favoriteExercises, setFavoriteExercises] = useState([]);
   const [exercises, setExercises] = useState([]);
   const [bodyPart, setBodyPart] = useState('all');
 
@@ -14,6 +16,7 @@ const Home = () => {
       <HeroBanner />
       <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+      <Favorites setFavoriteExercises={setFavoriteExercises} favoriteExercises={favoriteExercises} bodyPart={bodyPart}/>
     </Box>
   );
 };
